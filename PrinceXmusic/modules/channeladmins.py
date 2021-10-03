8bed903abb98dfd6d47c84f5da26020939f3b4e0
Aryan <84660211+stark-Prince@users.noreply.github.com>
2021-10-03 16:49:45 +0530

    Update channeladmins.py

--- PrinceXmusic/modules/channeladmins.py
+++ PrinceXmusic/modules/channeladmins.py
@@ -14,59 +14,66 @@ from PrinceXmusic.services.callsmusic import callsmusic
 from PrinceXmusic.services.queues import queues
 
 
-@Client.on_message(filters.command(["channelpause","cpause"]) & filters.group & ~filters.edited)
+@Client.on_message(
+    filters.command(["channelpause", "cpause"]) & filters.group & ~filters.edited
+)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
     try:
-      conchat = await _.get_chat(message.chat.id)
-      conid = conchat.linked_chat.id
-      chid = conid
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
     except:
-      await message.reply("Chat not linked I guess")
-      return    
+        await message.reply("Is chat even linked")
+        return
     chat_id = chid
-    if (chat_id not in callsmusic.active_chats) or (
-        callsmusic.active_chats[chat_id] == "paused"
-    ):
-        await message.reply_text("❗ Nothing is playing!")
-    else:
-        callsmusic.pause(chat_id)
+    (
         await message.reply_text("▶️ Paused!")
+    ) if (
+        callsmusic.pause(chat_id)
+    ) else (
+        await message.reply_text("❗ Nothing is playing!")
+    )
 
 
-@Client.on_message(filters.command(["channelresume","cresume"]) & filters.group & ~filters.edited)
+@Client.on_message(
+    filters.command(["channelresume", "cresume"]) & filters.group & ~filters.edited
+)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
     try:
-      conchat = await _.get_chat(message.chat.id)
-      conid = conchat.linked_chat.id
-      chid = conid
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
     except:
-      await message.reply("Chat not linked I guess")
-      return    
+        await message.reply("Is chat even linked")
+        return
     chat_id = chid
-    if (chat_id not in callsmusic.active_chats) or (
-        callsmusic.active_chats[chat_id] == "playing"
-    ):
-        await message.reply_text("❗ Nothing is paused!")
-    else:
+    (
+       await message.reply_text("⏸ Resumed!")
+    ) if (
         callsmusic.resume(chat_id)
-        await message.reply_text("⏸ Resumed!")
+    ) else (
+        await message.reply_text("❗ Nothing is paused!")
+    )
+        
+    
 
-
-@Client.on_message(filters.command(["channelend","cend"]) & filters.group & ~filters.edited)
+@Client.on_message(
+    filters.command(["channelend", "cend"]) & filters.group & ~filters.edited
+)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
     try:
-      conchat = await _.get_chat(message.chat.id)
-      conid = conchat.linked_chat.id
-      chid = conid
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
     except:
-      await message.reply("Chat not linked I guess")
-      return    
+        await message.reply("Is chat even linked")
+        return
     chat_id = chid
     if chat_id not in callsmusic.active_chats:
         await message.reply_text("❗ Nothing is streaming!")
@@ -80,18 +87,20 @@ async def stop(_, message: Message):
         await message.reply_text("❌ Stopped streaming!")
 
 
-@Client.on_message(filters.command(["channelskip","cskip"]) & filters.group & ~filters.edited)
+@Client.on_message(
+    filters.command(["channelskip", "cskip"]) & filters.group & ~filters.edited
+)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
     global que
     try:
-      conchat = await _.get_chat(message.chat.id)
-      conid = conchat.linked_chat.id
-      chid = conid
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
     except:
-      await message.reply("Chat not linked I guess")
-      return    
+        await message.reply("Is chat even linked")
+        return
     chat_id = chid
     if chat_id not in callsmusic.active_chats:
         await message.reply_text("❗ Nothing is playing to skip!")
@@ -101,10 +110,7 @@ async def skip(_, message: Message):
         if queues.is_empty(chat_id):
             await callsmusic.stop(chat_id)
         else:
-            await callsmusic.set_stream(
-                chat_id, 
-                queues.get(chat_id)["file_path"]
-            )
+            await callsmusic.set_stream(chat_id, queues.get(chat_id)["file"])
 
     qeue = que.get(chat_id)
     if qeue:
@@ -112,18 +118,76 @@ async def skip(_, message: Message):
     if not qeue:
         return
     await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+    
+    
+@Client.on_message(
+    filters.command(["channelmute", "cmute"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def mute(_, message: Message):
+    global que
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return 
+    chat_id = chid
+    result = await callsmusic.mute(chat_id)
+    (
+        await message.reply_text("✅ Muted")
+    ) if (
+        result == 0
+    ) else (
+        await message.reply_text("❌ Already muted")
+    ) if (
+        result == 1
+    ) else (
+        await message.reply_text("❌ Not in call")
+    )
+        
+        
+@Client.on_message(
+    filters.command(["channelunmute", "cunmute"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def unmute(_, message: Message):
+    global que
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return 
+    chat_id = chid
+    result = await callsmusic.unmute(chat_id)
+    (
+        await message.reply_text("✅ Unmuted")
+    ) if (
+        result == 0
+    ) else (
+        await message.reply_text("❌ Not muted")
+    ) if (
+        result == 1
+    ) else (
+        await message.reply_text("❌ Not in call")
+    )
 
 
 @Client.on_message(filters.command("channeladmincache"))
 @errors
 async def admincache(client, message: Message):
     try:
-      conchat = await client.get_chat(message.chat.id)
-      conid = conchat.linked_chat.id
-      chid = conid
+        conchat = await client.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
     except:
-      await message.reply("Chat not linked I guess")
-      return
+        await message.reply("Is chat even linked")
+        return
     set(
         chid,
         [
@@ -131,4 +195,4 @@ async def admincache(client, message: Message):
             for member in await conchat.linked_chat.get_members(filter="administrators")
         ],
     )
-    await message.reply_text("✅ I have successfully refreshed cache")
+    await message.reply_text("❇️ Admin cache refreshed!")

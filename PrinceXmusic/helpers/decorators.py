70bfd26258e387a68b96c25a20b6460edfe42c9b
Aryan <84660211+stark-Prince@users.noreply.github.com>
2021-10-09 17:27:12 +0530

    Update decorators.py

--- PrinceXmusic/helpers/decorators.py
+++ PrinceXmusic/helpers/decorators.py
@@ -29,3 +29,17 @@ def authorized_users_only(func: Callable) -> Callable:
                 return await func(client, message)
 
     return decorator
+
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+    return decorator
